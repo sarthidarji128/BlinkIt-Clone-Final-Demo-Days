4455560e04807192921ee984c5cef12916f651b0
sar12.8thi <142773841+sarthidarji128@users.noreply.github.com>
2024-06-22 17:37:52 +0530

    Update BrandPromotion.jsx

--- Blink-It/src/components/BrandPromotion.jsx
+++ Blink-It/src/components/BrandPromotion.jsx
@@ -5,7 +5,7 @@ import Feat3 from '../assets/images/promo-feat-3.avif';
 import Feat4 from '../assets/images/promo-feat-4.webp';
 import AppStoreLogo from '../assets/images/app-store.webp';
 import PlayStoreLogo from '../assets/images/play-store.webp';
-
+import './BrandPromotion.css'
 
 
 const PromoFeature = (props) => {
@@ -23,20 +23,10 @@ const BrandPromotion = () => {
     <section className="py-6 mt-8">
       <div className="_container">
         <div className="flex flex-col gap-8 lg:border-t _border-muted lg:pt-20">
-          <div className="_bg-shade-1 py-10 px-4 rounded-2xl">
+          <div className="py-10 px-4 rounded-2xl">
             <div className="lg:flex items-center justify-around h-full">
               <div className="hidden lg:flex flex-col justify-start w-[480px] relative">
-                <div className="absolute">
-                  <img
-                    src="/phone.webp"
-                    alt=""
-                    className="max-h-[500px] -mt-20 -ml-4"
-                  />
-                </div>
                 <div className="translate-y-32 -translate-x-16">
-                  <div className="_swinging">
-                    <img src="/bike.png" alt="" className="h-[360px]" />
-                  </div>
                 </div>
               </div>
               <div className="flex flex-col justify-center">
@@ -46,22 +36,22 @@ const BrandPromotion = () => {
                 <div className="text-lg space-y-2">
                   <div className="flex items-center">
                     <IoCheckmarkCircle
-                      size={24}
+                      size={20}
                       className="text-green-600 mr-3"
                     />{' '}
                     <span>Miss live order tracking</span>
                   </div>
                   <div className="flex items-center">
                     <IoCheckmarkCircle
-                      size={24}
+                      size={20}
                       className="text-green-600 mr-3"
                     />{' '}
                     <span>Miss latest feature updates</span>
                   </div>
                 </div>
-                <div className="hidden lg:flex items-center _bg-shade-2 p-3 mt-6 gap-3 rounded-xl">
+                <div className="hidden lg:flex items-center  p-3 mt-6 gap-3 rounded-xl">
                   <div>
-                    <img src="/qrcode.png" alt="" width={105} height={105} />
+                    <img src="https://blink-it-by-arshad-gaurav-ayush-rohit-shubham.netlify.app/images/Profile.png" alt="" width={105} height={50} />
                   </div>
                   <div className="flex flex-col justify-center">
                     <p className="font-extrabold text-lg max-w-[300px] _text-default">
@@ -76,38 +66,13 @@ const BrandPromotion = () => {
                   <h4 className="font-semibold text-md leading-none mb-4">
                     Download the BlinkIt App Now
                   </h4>
-                  <div className="flex flex-col xs:flex-row gap-2">
-                    <div className="xs:flex-1 h-12 rounded cursor-pointer overflow-hidden bg-[#213b50]">
-                      <img
-                        src={AppStoreLogo}
-                        alt="App store"
-                        className="h-full w-full object-contain"
-                      />
-                    </div>
-                    <div className="xs:flex-1 h-12 rounded cursor-pointer overflow-hidden bg-[#213b50]">
-                      <img
-                        src={PlayStoreLogo}
-                        alt="Play store"
-                        className="h-full w-full object-contain"
-                      />
-                    </div>
-                  </div>
                 </div>
               </div>
             </div>
           </div>
-          <div className="grid grid-cols-1 xs:grid-cols-2 lg:grid-cols-4 items-start gap-2 sm:gap-4 xl:gap-10 mt-6">
-            {allFeatures.map((feat, i) => (
-              <PromoFeature key={i} {...feat} />
-            ))}
-          </div>
           <div className="border-b _border-light pt-2 pb-10">
             <p className="text-sm _text-default">
               "BlinkIt" is owned & managed by "BlinkIt Commerce Private Limited"
-              (formerly known as Loafers Inc Private Limited) and is not
-              related, linked or interconnected in whatsoever manner or nature,
-              to "LOAFER.COM" which is a real estate services business operated
-              by "Blackstone Consultancy Services Fake Limited".
             </p>
           </div>
         </div>
