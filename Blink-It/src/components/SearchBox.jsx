7325696776de40ad56c5d7b13d908aafb59886a5
sar12.8thi <142773841+sarthidarji128@users.noreply.github.com>
2024-06-22 17:46:51 +0530

    Update SearchBox.jsx

--- Blink-It/src/components/SearchBox.jsx
+++ Blink-It/src/components/SearchBox.jsx
@@ -1,17 +1,19 @@
+import React from 'react';
 import { FiSearch } from 'react-icons/fi';
+import './SearchBox.css';  
+
 
 const SearchBox = () => {
   return (
-    <div className="_searchbox relative">
-      <FiSearch className="absolute top-1/2 -translate-y-1/2 left-4 text-gray-400" size={24} />
+    <div className="searchbox-relative">
+      <FiSearch className="absolute-icon" size={24} />
       <input
         type="text"
         placeholder="Search for products"
-        className="outline-none w-full text-[14px]"
+        className="search-input"
       />
     </div>
   );
 };
 
 export default SearchBox;
-
