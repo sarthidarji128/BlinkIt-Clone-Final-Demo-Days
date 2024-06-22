8713c7d98f2d34afc577df40301e03718e044656
sar12.8thi <142773841+sarthidarji128@users.noreply.github.com>
2024-06-22 17:56:31 +0530

    Update CategoriesList.jsx

--- Blink-It/src/components/home/CategoriesList.jsx
+++ Blink-It/src/components/home/CategoriesList.jsx
@@ -1,21 +1,95 @@
-import Categories from '../../lib/data/categories.json';
-
-const CategoriesList = (props) => {
+import React from 'react';
+import './CategoriesList.css'
+const CategoriesList = () => {
   return (
     <section className="my-4">
-      <div className="grid grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10 my-2">
-        {Categories.map((c) => (
-          <div key={c.id} className="h-48">
+      <div className="grids grid-cols-2 xs:grid-cols-3 sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8 xl:grid-cols-10 my-2">
+          <div className="h-48">
             <img
-              src={`categories/${c.coverFile}`}
-              className="mx-auto h-full w-full object-contain"
-              alt={c.title}
+              src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-12/paan-corner_web.png"
+              className="cover-img mx-auto h-full w-full object-contain"
+            />
+            <img
+              src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-2_10.png"
+              className="cover-img mx-auto h-full w-full object-contain"
+            />
+            <img
+              src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-3_9.png"
+              className="cover-img mx-auto h-full w-full object-contain"
+            />
+             <img
+              src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-4_9.png"
+              className="cover-img mx-auto h-full w-full object-contain"
+            />
+            <img
+              src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-5_4.png"
+              className="cover-img mx-auto h-full w-full object-contain"
+            />
+            <img
+              src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-6_5.png"
+              className="cover-img mx-auto h-full w-full object-contain"
+            />
+            <img
+              src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-7_3.png"
+              className="cover-img mx-auto h-full w-full object-contain"
+            />
+            <img
+              src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-8_4.png"
+              className="cover-img mx-auto h-full w-full object-contain"
+            />
+            <img
+              src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-9_3.png"
+              className="cover-img mx-auto h-full w-full object-contain"
+            />
+            <img
+              src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-10.png"
+              className="cover-img mx-auto h-full w-full object-contain"
+            />
+            <img
+              src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-11.png"
+              className="cover-img mx-auto h-full w-full object-contain"
+            />
+            <img
+              src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-12.png"
+              className="cover-img mx-auto h-full w-full object-contain"
+            />
+            <img
+              src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-13.png"
+              className="cover-img mx-auto h-full w-full object-contain"
+            />
+            <img
+              src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-14.png"
+              className="cover-img mx-auto h-full w-full object-contain"
+            />
+            <img
+              src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-15.png"
+              className="cover-img mx-auto h-full w-full object-contain"
+            />
+            <img
+              src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-16.png"
+              className="cover-img mx-auto h-full w-full object-contain"
+            />
+            <img
+              src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-17.png"
+              className="cover-img mx-auto h-full w-full object-contain"
+            />
+            <img
+              src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-18.png"
+              className="cover-img mx-auto h-full w-full object-contain"
+            />
+            <img
+              src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-19.png"
+              className="cover-img mx-auto h-full w-full object-contain"
+            />
+            <img
+              src="https://cdn.grofers.com/cdn-cgi/image/f=auto,fit=scale-down,q=70,metadata=none,w=270/layout-engine/2022-11/Slice-20.png"
+              className="cover-img mx-auto h-full w-full object-contain"
             />
           </div>
-        ))}
       </div>
     </section>
   );
 };
 
 export default CategoriesList;
+
