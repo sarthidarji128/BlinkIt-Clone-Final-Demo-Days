2cab1449f8799c1884f51b2a1f1ca8cbc9a49bd5
sar12.8thi <142773841+sarthidarji128@users.noreply.github.com>
2024-06-22 18:00:47 +0530

    Update Header.jsx

--- Blink-It/src/components/shared/Header.jsx
+++ Blink-It/src/components/shared/Header.jsx
@@ -3,32 +3,32 @@ import { Link } from 'react-router-dom';
 import { CartButton } from '../cart';
 import LocationPicker from '../LocationPicker';
 import SearchBox from '../SearchBox';
-
+import './Header.css';
 const Header = () => {
   return (
     <header className="_nav px-2 sm:px-0">
       <div className="_header sm:flex h-full">
         <div className="hidden sm:flex max-w-[150px] md:max-w-[178px] w-full cursor-pointer sm:hover:bg-gray-50 items-center justify-center border-r _border-light">
           <Link to={'/'}>
-            <span className="font-black text-[32px] md:text-[38px] text-yellow-400 tracking-tight">
-              Blink<strong className="text-green-600">It</strong>
+            <span className="name">
+              BlinkIt
             </span>
           </Link>
         </div>
-        <div className="w-full sm:w-[240px] xl:w-[320px] py-4 px-1 sm:p-0 _header_loc flex items-center sm:justify-center cursor-pointer sm:hover:bg-gray-50">
+        <div className="w-full sm:w-[240px] xl:w-[320px] py-4 px-1 sm:p-0 _header_loc flex items-center sm:justify-center cursor-pointer">
           <LocationPicker />
         </div>
         <div className="flex-1 relative _header_search">
           <SearchBox />
         </div>
-        <div className="flex items-center _header_login justify-center cursor-pointer sm:hover:bg-gray-50 max-w-[80px] lg:max-w-[160px] w-full ">
-          <span className="font-medium _text-default hidden sm:block">
-            Login
-          </span>
-          <span className="sm:hidden _text-default">
-            <FaRegUser size={22} />
-          </span>
-        </div>
+        <div className="login-button flex items-center justify-center cursor-pointer sm:hover:bg-gray-50 max-w-[80px] lg:max-w-[160px] w-full">
+      <span className="font-medium text-default hidden sm:block">
+        <a href='_blank' className="login-link">Login</a>
+      </span>
+      <span className="sm:hidden text-default">
+        <FaRegUser className="icon-default" size={22}/>
+      </span>
+    </div>
         <div className="py-2 hidden md:flex h-full items-center mr-8 ml-3">
           <CartButton />
         </div>
